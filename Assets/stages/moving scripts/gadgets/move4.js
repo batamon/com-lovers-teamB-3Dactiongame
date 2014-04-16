b4dbf6d758480da1a2b2d95f1972365f8b854818
batamon <merpus116kirby@yahoo.co.jp>
2014-04-16 18:03:56 +0900

    stage2完成

---
+++ Assets/stages/moving scripts/gadgets/move4.js
@@ -0,0 +1,9 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+  transform.position = Vector3(transform.position.x,transform.position.y+Mathf.Sin (Time.frameCount *  0.035) * 0.01,transform.position.z);
+}
